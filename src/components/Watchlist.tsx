a17bebaffeac299269950ddd25fb36df5e6910ba
ksony <KamalS4@hexaware.com>
2026-02-21 22:42:16 +0530

    feat(filter and Search): Added search and filter ability

--- src/components/Watchlist.tsx
+++ src/components/Watchlist.tsx
@@ -3,10 +3,27 @@ import genreids, { IMAGE_BASE_URL } from "../constant";
 
 function Watchlist() {
   const [watchList, setWatchlist] = useState([]);
+  const [search, setSearch] = useState('');
+  const [genreList, setGenresList] = useState(['All Genre', 'Action', 'Suspense','Thriller']);
+  const [currentGenre, setCurrentGenre] = useState('All Genre');
+
   useEffect(() => {
     const watchListFromStorage = JSON.parse(localStorage.getItem("watchlist"));
     setWatchlist(watchListFromStorage);
   }, []);
+
+  useEffect(() => {
+    const watchListFromStorage = JSON.parse(localStorage.getItem("watchlist"));
+    let temp = watchListFromStorage.map(listItem => {
+      return genreids[listItem.genre_ids[0]];
+    })
+    temp = new Set(temp);
+    setGenresList(['All Genre', ...temp]);
+  })
+
+  const onSearchAction = (e) => {
+    setSearch(e.target.value);
+  };
 
   const List = ({ movie }) => {
     return (
@@ -22,7 +39,7 @@ function Watchlist() {
             </div>
             <div className="ml-4">
               <div className="text-sm font-medium text-gray-900 dark:text-white">
-              {movie.title}
+                {movie.title}
               </div>
             </div>
           </div>
@@ -31,35 +48,63 @@ function Watchlist() {
           {movie.vote_average}
         </td>
         <td className="px-6 py-2 whitespace-nowrap">
-            <div className="">
-              <div className="text-sm font-medium text-gray-900 dark:text-white">
-                {movie.popularity}
-              </div>
+          <div className="">
+            <div className="text-sm font-medium text-gray-900 dark:text-white">
+              {movie.popularity}
             </div>
+          </div>
         </td>
         <td className="px-6 py-2 whitespace-nowrap">
-            <div className="">
-              <div className="text-sm font-medium text-gray-900 dark:text-white">
-                {genreids[movie.genre_ids[0]]}
-              </div>
+          <div className="">
+            <div className="text-sm font-medium text-gray-900 dark:text-white">
+              {genreids[movie.genre_ids[0]]}
             </div>
+          </div>
         </td>
       </tr>
     );
   };
 
   const sortAscending = () => {
-    const watchListAsc = watchList.sort((a,b) => a.vote_average - b.vote_average);
+    const watchListAsc = watchList.sort(
+      (a, b) => a.vote_average - b.vote_average
+    );
     setWatchlist([...watchListAsc]);
-  }
+  };
 
   const sortDesending = () => {
-    const watchListDesc = watchList.sort((a,b) => b.vote_average - a.vote_average);
+    const watchListDesc = watchList.sort(
+      (a, b) => b.vote_average - a.vote_average
+    );
     setWatchlist([...watchListDesc]);
+  };
+
+  const onFilterAction = (genre) => {
+    setCurrentGenre(genre);
   }
 
   return (
     <div className="dark:bg-gray-900 p-4">
+        <div className="flex justify-between m-4">
+          {genreList.map((gl) => {
+            const isActive = currentGenre == gl;
+            const baseStyles = 'flex justify-center items-center h-[3rem] w-[8rem] rounded-lg text-white font-bold mx-4 text-wrap hover:cursor-pointer';
+            const bgColor = isActive ? 'bg-blue-400' : 'bg-gray-400/50'
+            return (
+              
+              <div key={gl} className={`${baseStyles} ${bgColor}`} onClick={() => onFilterAction(gl)}>{gl}</div>
+            )
+          })}
+        </div>
+       <div className="relative p-4">
+          <input  
+            type="text"
+            className="block w-full p-3 ps-9 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand shadow-xs placeholder:text-body"
+            placeholder="Search"
+            value={search}
+            onChange={onSearchAction}
+          />
+        </div>
       <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
         <thead className="bg-gray-50 dark:bg-gray-800">
           <tr>
@@ -67,9 +112,19 @@ function Watchlist() {
               Name
             </th>
             <th className="px-6 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider flex justify-evenly">
-              <div className="hover:cursor-pointer mr-1" onClick={sortAscending}>🔼</div>
+              <div
+                className="hover:cursor-pointer mr-1"
+                onClick={sortAscending}
+              >
+                🔼
+              </div>
               Ratings
-              <div className="hover:cursor-pointer ml-1" onClick={sortDesending}>🔽</div>
+              <div
+                className="hover:cursor-pointer ml-1"
+                onClick={sortDesending}
+              >
+                🔽
+              </div>
             </th>
             <th className="px-6 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
               Popularity
@@ -80,9 +135,18 @@ function Watchlist() {
           </tr>
         </thead>
         <tbody className="bg-white dark:bg-gray-800 divide-y divide-gray-200 dark:divide-gray-700">
-          {watchList?.map((wl) => (
-            <List movie={wl} key={wl.id} />
-          ))}
+          {watchList
+            .filter((wl) => {
+              if(currentGenre == 'All Genre') {
+                return true;
+              } else {
+                return genreids[wl.genre_ids[0]] == currentGenre;
+              }
+            })
+            .filter((wl) => wl.title.includes(search))
+            .map((wl) => (
+              <List movie={wl} key={wl.id} />
+            ))}
         </tbody>
       </table>
     </div>
