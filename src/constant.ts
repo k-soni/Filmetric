d815409937c1d3dc8440bb7eaa1b0c04baca2882
ksony <KamalS4@hexaware.com>
2026-02-23 09:25:18 +0530

    pushed the constants

---
+++ src/constant.ts
@@ -0,0 +1,28 @@
+export const READ_ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOTRjN2RhYTA1MDQzNzRkMzljZTk2MGQwZDIxZDE3NyIsIm5iZiI6MTc3MTU1NTgyMC4xMjEsInN1YiI6IjY5OTdjYmVjYjMyOGQzZTU4ZjY2NjUxMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.n-jYogWasOikZXdFIjCYQey9Omj1avClcPfHlGHG7OY";
+export const API_KEY = "c94c7daa0504374d39ce960d0d21d177";
+export const BASE_URL = "https://api.themoviedb.org/3";
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original';
+
+const genreids = {
+    28: "Action",
+    12: "Adventure",
+    16: "Animation",
+    35: "Comedy",
+    80: "Crime",
+    99: "Documentary",
+    18: "Drama",
+    10751: "Family",
+    14: "Fantasy",
+    36: "History",
+    27: "Horror",
+    10402: "Music",
+    9648: "Mystery",
+    10749: "Romance",
+    878: "Sci-Fi",
+    10770: "TV",
+    53: "Thriller",
+    10752: "War",
+    37: "Western",
+    };
+
+    export default genreids;
